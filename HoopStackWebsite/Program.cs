84039a317538ce5334025e98527feebf444172ed
Nikita Andreikin <nandreik@gmu.edu>
2020-10-25 22:35:59 -0400

    heroku attempts

--- HoopStackWebsite/Program.cs
+++ HoopStackWebsite/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 using HoopStackWebsite.Solver;
+using Microsoft.Extensions.Configuration; //added for heroku deployment attempt 
 
 namespace HoopStackWebsite
 {
@@ -15,7 +16,19 @@ namespace HoopStackWebsite
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run(); //original main 
+
+            //added for heroku deployment
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(System.IO.Directory.GetCurrentDirectory())
+                .UseConfiguration(config)
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
